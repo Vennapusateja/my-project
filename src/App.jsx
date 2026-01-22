de02161e6850e78946b32d06c069d0b730021642
vennapusa teja <120013964+Vennapusateja@users.noreply.github.com>
2026-01-22 15:24:27 +0530

    Create initial App component with landing page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing">
+      <div className="overlay">
+        <h1>Paradise Nursery</h1>
+        <p>Bring nature into your home</p>
+        <button className="get-started">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
